c6001d035d66afaacc42216c8f957a1973768e68
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2024-06-03 14:40:51 +0200

    fix(ci): smarter handling of stdout for unittests

--- tasks/unit_tests.py
+++ tasks/unit_tests.py
@@ -9,6 +9,6 @@ def invoke_unit_tests(ctx):
     Run the unit tests on the invoke tasks
     """
     ctx.run(
-        f"'{sys.executable}' -m unittest discover -s tasks -p '*_tests.py'",
+        f"'{sys.executable}' -m unittest discover -s tasks -p '*_tests.py' -b",
         env={"GITLAB_TOKEN": "fake_token", "INVOKE_UNIT_TESTS": "1"},
     )
